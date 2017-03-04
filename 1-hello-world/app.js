0447612a18006f2505d06a71e8ab73f4a95a599e
Kishan Trivedi <kishantr@usc.edu>
2017-03-04 12:19:45 -0800

    ML WORKS

--- 1-hello-world/app.js
+++ 1-hello-world/app.js
@@ -21,13 +21,46 @@ var router = express.Router();
 
 app.use(express.static('test'));
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/', router);
+
+
+
+// Imports the Google Cloud client library
+const Vision = require('@google-cloud/vision');
+
+// Your Google Cloud Platform project ID
+const projectId = 'picasso-160507';
+
+// Instantiates a client
+const visionClient = Vision({
+  projectId: projectId
+});
+
 
 router.get('/', (req, res) => {
   res.sendfile('test/index.html');
 });
 
+app.post('/', function(req, res){
+  // The name of the image file to annotate
+  const fileName = 'test/giraffe.jpg';
 
-app.use('/', router);
+
+  // Performs label detection on the image file
+  visionClient.detectLabels(fileName)
+    .then((results) => {
+      const labels = results[0];
+
+      console.log('Labels:');
+      labels.forEach((label) => console.log(label));
+    });
+});
+
+
 
 if (module === require.main) {
   // [START server]
